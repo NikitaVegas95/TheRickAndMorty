59b4b05df129a47558427f55d9224dec9e74395c
Isaev.N <nikitavegas95@gmail.com>
2024-06-18 10:17:15 +0300

    feature/filteredCharacter

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const characterCard = document.querySelector('.character__wrapper')
+const searchInput = document.getElementById('input-id')
 const baseURL = 'https://rickandmortyapi.com/api/character'
 
 const fetchAllCharacter = async () => {
@@ -11,23 +12,46 @@ const fetchAllCharacter = async () => {
     return await response.json()
 }
 
-fetchAllCharacter()
-    .then(results => {
-        results.results.map((results) => {
-            characterCard.innerHTML +=
-                `<div class="character__card">
-                    <p class="character__title">${results.name}</p>
-                    <img class="character__img" src="${results.image}" alt="">
-                    <div class="character__text-wrapper">
-                        <p class="character__text">Specie: <span>${results.species}</span></p>
-                        <p class="character__text">State: <span>${results.status}</span></p>
-                        <p class="character__text">Location: <span>${results.location.name}</span></p>
-                    </div>
+const displayCharacters = (characters) => {
+    characterCard.innerHTML = ''
+    characters.map((character) => {
+        characterCard.innerHTML +=
+            `<div class="character__card">
+                <p class="character__title">${character.name}</p>
+                <img class="character__img" src="${character.image}" alt="${character.name}">
+                <div class="character__text-wrapper">
+                    <p class="character__text">Specie: <span>${character.species}</span></p>
+                    <p class="character__text">State: <span>${character.status}</span></p>
+                    <p class="character__text">Location: <span>${character.location.name}</span></p>
                 </div>
-                `
-        })
+            </div>
+            `
     })
+}
 
+const filterCharactersByName = (characters, name) => {
+    return characters.filter(character => {
+        return (`
+        ${character.name} 
+        ${character.status} 
+        ${character.location.name} 
+        ${character.species}
+        `).toLowerCase().includes(name.toLowerCase().trim())
+    })
+}
 
+const getFilteredCharacters = async () => {
+    const response = await fetchAllCharacter()
+    displayCharacters(response.results)
+    searchInput.addEventListener('input', (event) => {
+        const filteredCharacters = filterCharactersByName(response.results, event.target.value)
+        displayCharacters(filteredCharacters)
+    })
+}
+
+getFilteredCharacters()
+    .catch(error => {
+        error('Ошибка запроса')
+    })
 
 
